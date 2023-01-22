131e03d56f49095bff0074d35241643dfff8282b
Bulzay <goawaylol@yandex.ru>
2023-01-22 23:36:22 +0300

    models reworked a bit

--- backend/chat_proj/chat/models.py
+++ backend/chat_proj/chat/models.py
@@ -3,9 +3,12 @@ from django.contrib.auth.models import User
 
 
 class Author(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     username = models.CharField(max_length=64)
-    picture = models.FileField(upload_to=None)
+    picture = models.ImageField(upload_to='profile_pics/', blank=True)
+
+    def __str__(self):
+        return f'Author {self.username}'
 
 
 class ChatRoom(models.Model):
@@ -15,15 +18,20 @@ class ChatRoom(models.Model):
     description = models.CharField(max_length=255)
     last_activity = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f'Chat {self.name}'
+
 
 class Message(models.Model):
     author = models.ForeignKey(Author, on_delete=models.DO_NOTHING)
     chat = models.ForeignKey(ChatRoom, on_delete=models.CASCADE)
     content = models.TextField()
-    pinned = models.FileField(upload_to=None)
+    pinned = models.ImageField(upload_to='pinned_pics/', blank=True)
+
+    def __str__(self):
+        return f'Msg {self.content[:20]}'
 
 
 class UserChat(models.Model):
     user = models.ForeignKey(Author, on_delete=models.CASCADE)
     chat = models.ForeignKey(ChatRoom, on_delete=models.CASCADE)
-
